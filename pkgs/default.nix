693a3f429931e2f5fa096252162f0f7f0829dcb4
Sam Hug <s@m-h.ug>
2018-12-31 23:20:36 +0000

    add etlcmd

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -4,6 +4,7 @@ let
   inherit (self) callPackage;
 in
 {
+  etlcmd = callPackage ./etlcmd {};
   gobgp = callPackage ./gobgp {};
   ndm = callPackage ./ndm {};
   zfs-remote-keyloader = callPackage ./zfs-remote-keyloader {};
