5e12f500499bcf8bea98cba4432f8b99f27f3f07
Sam Hug <s@m-h.ug>
2019-10-14 19:07:58 -0700

    zfs-remote-keyloader: update deps

--- pkgs/zfs-remote-keyloader/default.nix
+++ pkgs/zfs-remote-keyloader/default.nix
@@ -5,16 +5,16 @@
 
 buildGoModule rec {
   name = "zfs-remote-keyloader-${version}";
-  version = "0.0.1";
+  version = "0.0.2";
 
   src = fetchFromGitHub {
     owner = "samhug";
     repo = "zfs-remote-keyloader";
     rev = "v${version}";
-    sha256 = "1r1g6s1057yb40gjy83d6smgbj3r8crfrnxx5n8ksr3vfylx4jg7";
+    sha256 = "1825fckb7s7zagvs7vgy3n7rrssp2p0a5dkhrnyp4k7fdg7dq4ii";
   };
 
-  modSha256 = "1j3vgmn64shy1v73c1pz1qm90b837mlbjz3gaspn2fi2akyab53f";
+  modSha256 = "0c7l14gv8aj11v18rs1lc9gwfh7wcm569agf7z8d5i02aji2nk9g";
 
   subPackages = [ "." ];
 
