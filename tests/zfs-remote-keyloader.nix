4e31abbb7b53b1b8a57205a0e69c7d1edf48fbe8
Sam Hug <s@m-h.ug>
2019-11-24 12:09:34 -0800

    port zfs-remote-keyloader test to python

--- tests/zfs-remote-keyloader.nix
+++ tests/zfs-remote-keyloader.nix
@@ -1,10 +1,21 @@
-import <nixpkgs/nixos/tests/make-test.nix> {
+{ system ? builtins.currentSystem
+, config ? {}
+, pkgs ? import (import ./nix/pkgs-src.nix) { inherit system config; }
+}:
+
+with import "${import ./nix/pkgs-src.nix}/nixos/lib/testing-python.nix" { inherit system pkgs; };
+with pkgs.lib;
+let
+  overlay-src = ../.;
+
+in
+makeTest {
   name = "zfs-remote-keyloader";
 
   nodes = {
     server = { pkgs, lib, ... }: {
       imports = [
-        (import ../.)
+        overlay-src
       ];
       virtualisation.emptyDiskImages = [ 128 ];
 
@@ -32,37 +43,37 @@ import <nixpkgs/nixos/tests/make-test.nix> {
       zfsPool = "rpool";
     in
       ''
-        $server->start;
-        $client->start;
+        server.start()
+        client.start()
 
-        $server->waitForUnit("multi-user.target");
-        $server->succeed("modprobe zfs");
-        $server->succeed(
-          "udevadm settle",
-          "sgdisk --zap-all /dev/vdb",
-          "echo ${zfsEncryptionKey} | zpool create"
-            . " -O mountpoint=none"
-            . " -O encryption=aes-256-gcm"
-            . " -O keyformat=passphrase"
-            . " ${zfsPool} vdb",
-          "zfs unload-key ${zfsPool}",
-        );
+        server.wait_for_unit("multi-user.target")
+        server.succeed("modprobe zfs")
+        server.succeed(
+            "udevadm settle",
+            "sgdisk --zap-all /dev/vdb",
+            "echo ${zfsEncryptionKey} | zpool create"
+            + " -O mountpoint=none"
+            + " -O encryption=aes-256-gcm"
+            + " -O keyformat=passphrase"
+            + " ${zfsPool} vdb",
+            "zfs unload-key ${zfsPool}",
+        )
 
         # This should fail if the key isn't loaded
-        $server->fail("zfs unload-key ${zfsPool}");
+        server.fail("zfs unload-key ${zfsPool}")
 
-        $server->succeed("zfs-remote-keyloader --help");
-        $server->execute("zfs-remote-keyloader server --listen 0.0.0.0:3333 --dataset ${zfsPool} &");
-        $server->waitForOpenPort(3333);
+        server.succeed("zfs-remote-keyloader --help")
+        server.execute("zfs-remote-keyloader server --listen 0.0.0.0:3333 --dataset ${zfsPool} &")
+        server.wait_for_open_port(3333)
 
-        $client->waitForUnit("multi-user.target");
-        $client->succeed("curl http://server:3333/");
-        $client->succeed("curl -X POST -F 'decryption-key=${zfsEncryptionKey}' http://server:3333/");
+        client.wait_for_unit("multi-user.target")
+        client.succeed("curl http://server:3333/")
+        client.succeed("curl -X POST -F 'decryption-key=${zfsEncryptionKey}' http://server:3333/")
 
         # This should fail if the key is already loaded
-        $server->fail("zfs load-key ${zfsPool}");
+        server.fail("zfs load-key ${zfsPool}")
 
         # This should succeed if the key is already loaded
-        $server->succeed("zfs unload-key ${zfsPool}");
+        server.succeed("zfs unload-key ${zfsPool}")
       '';
 }
